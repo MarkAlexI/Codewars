d73506317ee3d855811ac359b0d4d1f5b9302adc
MarkAlexI <markovaleksandr108@gmail.com>
2025-04-10 17:33:36 +0300

    Add solution for '8kyu - Refactor Variable Names to Variable Keywords' on C#.

---
+++ C#/8kyu - Refactor Variable Names to Variable Keywords.cs
@@ -0,0 +1,16 @@
+public class VariableName
+{
+    public bool @bool = false;
+    public char @char = '\0';
+    public sbyte @sbyte = 0;
+    public byte @byte = 0;
+    public short @short = 0;
+    public ushort @ushort = 0;
+    public int @int = 0;
+    public uint @uint = 0;
+    public long @long = 0L;
+    public ulong @ulong = 0UL;
+    public float @float = 0f;
+    public double @double = 0.0;
+    public decimal @decimal = 0m;
+}
